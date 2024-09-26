a0f0de2064280b9cfea2e688df08444f61feb6d2
Kurrem <oscar.harmsen@gmail.com>
2024-09-26 09:31:49 +0200

    Created an operator function which takes an operator string and two variable values and runs the correct operation on the variables, based on the operator string.

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function calcMult (a, b) {
 function calcDiv (a, b) {
     return a / b;
 }
+
+function operator (operator, var1, var2) {
+    if (operator === '+') {
+        return calcAdd(var1, var2);
+    } else if (operator === '-') {
+        return calcSub(var1, var2);
+    } else if (operator === '*') {
+        return calcMult(var1, var2);
+    } else if (operator === '/') {
+        return calcDiv(var1, var2);
+    }
+}
+
+const var1 = 0;
+const var2 = 0;
+const operator = '';
