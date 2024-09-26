a59d668f4c6a1e0522e6dc7e44ba2f3eecca8744
Kurrem <oscar.harmsen@gmail.com>
2024-09-26 09:27:23 +0200

    Created basic functions for addition, subtraction, multiplication and division.

---
+++ script.js
@@ -0,0 +1,15 @@
+function calcAdd (a, b) {
+    return a + b;
+}
+
+function calcSub (a, b) {
+    return a - b;
+}
+
+function calcMult (a, b) {
+    return a * b;
+}
+
+function calcDiv (a, b) {
+    return a / b;
+}
