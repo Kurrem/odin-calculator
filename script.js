4e695a72df9241aac086a3f44e416238e06d7b90
Kurrem <oscar.harmsen@gmail.com>
2024-10-21 16:18:49 +0200

    The calculator is finished but not all of the extra credits. Will pause the task here to continue with the course instead.

--- script.js
+++ script.js
@@ -33,8 +33,62 @@ function operator (op, var1, var2) {
     }
 }
 
+// function checkForbidden(display) {
+//     currVal = display.innerHTML
+//     occurencesSingle = countOccurence(currVal, '.')
+//     if (occurencesSingle < 1) {
+
+//         return true
+//     }
+//     else return false
+// }
+
+// function countOccurence(stringToSplit, subString) {
+//     return stringToSplit.split(subString).length -1
+// }
+
 function addToDisplay(value) {
-    displayVar.innerHTML = displayVar.innerHTML + value;
+    dotButton = document.getElementById("dot")
+    // evalVal = displayVar.innerHTML.split(/[.\*+-/_]/)
+    // console.log(evalVal)
+
+    // if (value === '.') {
+    //     evalVal = displayVar.innerHTML.split(/[\*+-/]/)
+    //     console.log(evalVal)
+    //     test = evalVal[evalVal.length-1]
+    //     console.log(test)
+    //     if (test === ''){
+
+    //     }
+    // }
+    displayVar.innerHTML = displayVar.innerHTML + value
+    console.log(displayVar.innerHTML)
+    test123 = displayVar.innerHTML.split(/[\*+-/]/)
+    console.log(test123)
+    if (test123[test123.length-1] === '' && value === '.') {
+        dotButton.disabled = true;
+    } else dotButton.disabled = false;
+
+
+
+
+/**if value is a dot, check to see that the latest subset doest contain a dot already */
+
+
+    // if (displayVar.innerHTML.split('.').length -1 < 1 ) {
+    //     displayVar.innerHTML = displayVar.innerHTML + value
+    //     // dotButton.disabled = false;
+    //     // console.log("should be enabled")
+    //     // console.log(dotButton)
+    // } else {
+    //     // dotButton.disabled = true;
+    //     // console.log("should be disabled")
+    //     // console.log(dotButton)
+    // }
+
+    // if (checkForbidden(displayVar)) {
+    //     displayVar.innerHTML = displayVar.innerHTML + value;
+    // } else {alert('Too many .')}
 }
 
 function clearDisplay() {
@@ -52,11 +106,8 @@ function evalDisplay(toEval) {
     displayVar.innerHTML = result
 }
 
+/* Display */
 let displayVar = document.querySelector("#math-box-numbers");
-const var1 = 0;
-const var2 = 0;
-const op = '';
-
 
 /* Builds the numpad for the calculator */
 numpad = document.querySelector("#numpadNumbers");
